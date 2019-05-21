6627189c8e3766367e528308ea87416ebe8ddabd
Henrique Szostak <henriques@jmalucelliti.com.br>
2019-05-21 14:15:24 -0300

    First try - fail (:
    I didn´t like this approach to the problem, going to refactor the whole
    thing.

---
+++ src/components/range-slider/Track.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const Track = styled.div`
+  width: 100%
+  height: 20px;
+  background-color: #bb0909;
+`;
+
+export default Track;
---
+++ src/components/range-slider/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+import Handler from './Handler';
+import Track from './Track';
+
+const Wrapper = styled.div`
+  position: relative;
+  width: 100%;
+`;
+
+function RangeSlider() {
+
+  const onHandleUpdate = value => {
+    console.log(value);
+  }
+
+  return (
+    <Wrapper>
+      <Handler
+        onUpdate={onHandleUpdate}
+      />
+      <Track />
+    </Wrapper>
+  );
+}
+
+RangeSlider.propTypes = {
+  initialValue: PropTypes.number,
+  minValue: PropTypes.number,
+  maxValue: PropTypes.number,
+  step: PropTypes.number,
+  disabled: PropTypes.bool,
+}
+
+RangeSlider.defaultProps = {
+  initialValue: 0,
+  minValue: 0,
+  maxValue: 500,
+  step: 1,
+  disabled: false,
+}
+
+export default RangeSlider;
